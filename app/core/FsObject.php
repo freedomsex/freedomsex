4c956211c99b28a83e29d27d62040765f7a71cb0
tebaly <paygo51@gmail.com>
2015-01-21 13:13:39 +0300

    Опубликовано

---
+++ app/core/FsObject.php
@@ -0,0 +1,14 @@
+<?php     
+namespace core;
+
+class FsObject 
+{          
+        
+    public function __construct()
+    {
+     
+    }
+     
+   
+ 
+}
---
+++ index_one.php
@@ -0,0 +1,15 @@
+<?php  
+/**   
+ * Временный файл на время приведения движка к новой архитектуре
+ * 
+ */ 
+                 
+/*                                  
+error_reporting(7);
+ini_set('display_errors', true);      */  
+                         
+                         
+  header('Content-Type: text/html; charset=UTF-8');
+    
+  require_once 'app/bootstap.php'; 
+  
